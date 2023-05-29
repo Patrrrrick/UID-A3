3ca7a8267ef20cc24b64a9d9b4f95135b97539e6
Patrrrrick <yaochen2@student.unimelb.edu.au>
2023-05-29 16:13:04 +1000

    add function to mobile information section

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,15 @@
 function dropdownShow() {
     let dropdown = document.querySelector('.size-picker');
-    dropdown.classList.toggle("showDropdown")
+    dropdown.classList.toggle("showDropdown");
   }
+// JS for product information mobile
+var infoBtn = document.querySelector(".info-btn");
+var content = document.querySelector(".info-content-mobile");
+
+infoBtn.addEventListener('click', function() {
+  if (content.style.display === 'none') {
+    content.style.display = 'block';
+  } else {
+    content.style.display = 'none';
+  }
+});
