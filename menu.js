c5662e61398fb5840927231362bb0614d5696350
Ryan YUE 6 <126571525+RyanWang688@users.noreply.github.com>
2023-06-01 09:20:20 +1000

    add a icon change

--- menu.js
+++ menu.js
@@ -1,15 +1,19 @@
-function showMenu(menu) {
-    var menuElement = document.getElementById('dropMenu' + menu.charAt(0).toUpperCase() + menu.slice(1));
-    if(menuElement) {
-        menuElement.style.display = 'flex';
+document.getElementById('layoutbutton').addEventListener('click',function() {
+    var oldimage = document.getElementById('layoutbutton');
+    if (oldimage.scroll.match('images/icons/WechatIMG53762.png')) {
+        oldimage.src = 'UID-A3/images/icons/WechatIMG53761.png';
+    } else {
+        oldimage.src ='images/icons/WechatIMG53762.png';
+    }
+});
+
+
+
+function iconChange() {
+    var icon = document.getElementById('layoutbutton');
+    if (icon.src.match('Wechat')) {
+        icon.src = 'images/icons/layout.png'
+    } else {
+        icon.src ='images/icons/WechatIMG53762.png'
     }
 }
-
-function hideMenu(menu) {
-    var menuElement = document.getElementById('dropMenu' + menu.charAt(0).toUpperCase() + menu.slice(1));
-    if(menuElement) {
-        menuElement.style.display = 'none';
-    }
-}
-
-
